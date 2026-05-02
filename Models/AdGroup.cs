ff6e6394b1b2792cc86dcc708c502333c492d968
TKYS <agri066127@gmail.com>
2026-05-02 22:39:10 +0900

    Align README with current safety-scoped MVP and modes

---
+++ Models/AdGroup.cs
@@ -0,0 +1,7 @@
+namespace ManageAdTool.Models;
+
+public class AdGroup
+{
+    public string Name { get; init; } = string.Empty;
+    public string DistinguishedName { get; init; } = string.Empty;
+}
